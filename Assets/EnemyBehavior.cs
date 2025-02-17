5ef977fce7102f2fe36469636dd600f43408b529
brandoncrouse <musikaihd@gmail.com>
2025-02-16 23:40:05 -0500

    a* pathing

--- Assets/EnemyBehavior.cs
+++ Assets/EnemyBehavior.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyBehavior : Damageable
 {
-    
+    private AIPath path;
+    [SerializeField] float moveSpeed;
+    [SerializeField] Transform target;
+
+    protected override void Start()
+    {
+        base.Start();
+        path = GetComponent<AIPath>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        path.maxSpeed = moveSpeed;
+        path.destination = target.position;
+    }
 }
